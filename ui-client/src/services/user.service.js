2953de3cd8488b70a0b5bb9ff457f70eacdbeb4a
Miriam Bade <yukinomiriam@gmail.com>
2022-01-26 17:02:49 -0600

    Update user.service.js

--- ui-client/src/services/user.service.js
+++ ui-client/src/services/user.service.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-const API_URL = "http://localhost:3001/api/user/";
+
+const PORT = process.env.PORT || 3000;
+const API_URL = "http://localhost:" + PORT + "/api/user/";
 
 class UserService {
   getUserBalance(id) {
@@ -8,6 +10,21 @@ class UserService {
       .get(API_URL + "balance/" + id, { headers: authHeader() })
       .then((response) => {
         console.log("response: getUserBalance");
+        console.log(response.data);
+        return response.data;
+      });
+  }
+
+  updateUserBalance(id, balance) {
+    const newBalance = {
+      balance: balance,
+    };
+    console.log("updateUserBalance: " + API_URL + "balance/" + id);
+    console.log(newBalance);
+    return axios
+      .put(API_URL + "balance/" + id, newBalance, { headers: authHeader() })
+      .then((response) => {
+        console.log("response: updateUserBalance");
         console.log(response.data);
         return response.data;
       });
