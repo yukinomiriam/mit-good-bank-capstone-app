fd2ceb47b8900f0272961af37663bb119e757ac7
Miriam Bade <yukinomiriam@gmail.com>
2022-02-02 20:21:03 -0600

    Added Data Table to display all the users

--- ui-client/src/components/allData/AllData.js
+++ ui-client/src/components/allData/AllData.js
@@ -2,14 +2,40 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Card from "../Card";
-import RowTable from "./RowTable";
-
+import { MDBDataTableV5 } from "mdbreact";
 import UserService from "../../services/user.service";
 import EventBus from "../../common/EventBus";
 
 function AllData() {
   const [usersList, setUsersList] = useState([]);
   const { user: currentUser } = useSelector((state) => state.auth);
+  const columns = [
+    {
+      label: "Acct #",
+      field: "acct",
+      width: 150,
+    },
+    {
+      label: "First Name",
+      field: "firstName",
+      width: 150,
+    },
+    {
+      label: "Last Name",
+      field: "lastName",
+      width: 150,
+    },
+    {
+      label: "Email",
+      field: "email",
+      width: 150,
+    },
+    {
+      label: "Role",
+      field: "role",
+      width: 150,
+    },
+  ];
 
   useEffect(() => {
     //fetch all accounts
@@ -36,30 +62,46 @@ function AllData() {
     }
   }, []);
 
+  function buildRows() {
+    let tempRows = [];
+    if (usersList) {
+      for (const user of usersList) {
+        let t = {
+          acct: user.acct,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          role: user.roles,
+        };
+        tempRows.push(t);
+      }
+    }
+    return tempRows;
+  }
+  let rows = buildRows();
   return (
     <>
       {currentUser ? (
         <Card
           header="All Data"
           className="card brand-centered brand-margin-top"
-          maxWidth="40rem"
+          maxWidth="50rem"
           body={
             <>
-              <table className="brand-table">
-                <thead>
-                  <tr>
-                    <th className="brand-table-th-small">Acct #</th>
-                    <th className="brand-table-th-small">Name</th>
-                    <th className="brand-table-th-small">Email</th>
-                    <th className="brand-table-th-small">Roles</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {usersList.map((user, i) => (
-                    <RowTable data={user} key={i} />
-                  ))}
-                </tbody>
-              </table>
+              <div>
+                <MDBDataTableV5
+                  hover
+                  striped
+                  searchTop
+                  searchBottom={false}
+                  entriesOptions={[5, 10, 15]} //drop down for num records per page
+                  entries={5} //entries per page
+                  sorting={false}
+                  pagesAmount={5}
+                  paginationLabel={["Previous", "Next"]}
+                  data={{ columns, rows }}
+                />
+              </div>
             </>
           }
         />
